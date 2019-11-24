b7367ecae5b593f6baab691e016a83a91ac544af
QwertygidQ <Qwertygid@gmail.com>
2019-11-24 19:48:43 +0300

    A bit more testing

--- storymode.go
+++ storymode.go
@@ -15,6 +15,14 @@ func main() {
 		Name: "opt_b",
 		Text: "You chose option B!",
 		Options: map[string]string{
+			"Option C":             "opt_c",
+			"Finish your journey!": endArcName,
+		},
+	}
+	arc3 := &Arc{
+		Name: "opt_c",
+		Text: "You chose option C!",
+		Options: map[string]string{
 			"Finish your journey!": endArcName,
 		},
 	}
@@ -25,9 +33,13 @@ func main() {
 		Arcs: map[string]*Arc{
 			startArcName: arc1,
 			"opt_b":      arc2,
+			"opt_c":      arc3,
 		},
 		CurrentArc: arc1,
 	}
 
 	fmt.Printf("checkStory() output -- %s\n", story.checkStory())
+	fmt.Printf("currentArc name -- %s\n", story.CurrentArc.Name)
+	fmt.Printf("changeArc() output -- %s\n", story.changeArc("opt_b"))
+	fmt.Printf("currentArc name -- %s\n", story.CurrentArc.Name)
 }
