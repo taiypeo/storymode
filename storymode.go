e5dd6daec2b5d218fc7d7add63f1f492b294ebc6
QwertygidQ <Qwertygid@gmail.com>
2019-11-24 21:03:29 +0300

    Error handling in main()

--- storymode.go
+++ storymode.go
@@ -1,9 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	story, err := loadStory("examples/test_story.json")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		os.Exit(1)
+	}
+
 	fmt.Println(story)
-	fmt.Println(err)
 }
